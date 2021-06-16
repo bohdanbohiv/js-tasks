de2b27ecfa65789af4ea48224bd386da7fc18605
ostapalfavitskyi <71269318+ostapalfavitskyi@users.noreply.github.com>
2021-06-16 16:06:30 +0300

    Update tasks-01.js

--- tasks-01.js
+++ tasks-01.js
@@ -1,1 +1,26 @@
+//Grasshopper - Messi goals function: https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+//Grasshopper - Terminal game move function: https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
+}
+
+//Grasshopper - Personalized Message: https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+function greet(name, owner) {
+  if (name == owner) {
+    return "Hello boss";
+  }
+  return "Hello guest";
+}
+
+//Keep Hydrated!: https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+function litres(time) {
+  return Math.floor(time / 2);
+}
