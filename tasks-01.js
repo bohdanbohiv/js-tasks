6d50192f42f4b42a7cc68418e95ef963b987f1e5
bohdanbohiv <53075276+bohdanbohiv@users.noreply.github.com>
2021-06-16 16:31:40 +0300

    Update tasks-01.js

--- tasks-01.js
+++ tasks-01.js
@@ -13,7 +13,7 @@ function move(position, roll) {
 //Grasshopper - Personalized Message: https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
 function greet(name, owner) {
-  if (name == owner) {
+  if (name === owner) {
     return "Hello boss";
   }
   return "Hello guest";
