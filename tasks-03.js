0db4251576e6d41c4d0ad3744dc6e5a48f1d9965
Olhavavruk <85847178+Olhavavruk@users.noreply.github.com>
2021-06-23 15:16:32 +0300

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -13,3 +13,49 @@ function circleCircumference(circle) {
   var circumference = circle.radius * 2 * Math.PI;
   return circumference;
 }
+
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+// 3) Fun with ES6 Classes #2 - Animals and Inheritance
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.legs = 0;
+    this.status = status;
+    this.species = "shark";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "cat";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master = "") {
+    super();
+    this.master = master;
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "dog";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
