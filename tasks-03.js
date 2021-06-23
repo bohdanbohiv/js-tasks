8cf2a57fa1d927ae582c69778a06e14645724180
Olhavavruk <85847178+Olhavavruk@users.noreply.github.com>
2021-06-23 14:50:07 +0300

    Update tasks-03.js

--- tasks-03.js
+++ tasks-03.js
@@ -1,8 +1,15 @@
 // https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
-1) Find the smallest integer in the array codewars
+// 1) Find the smallest integer in the array codewars
 
 class SmallestIntegerFinder {
   findSmallestInt(args) {
     return Math.min(...args);
   }
+}
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+// 2) Geometry Basics: Circle Circumference in 2D
+
+function circleCircumference(circle) {
+  var circumference = circle.radius * 2 * Math.PI;
+  return circumference;
 }
