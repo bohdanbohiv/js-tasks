e79a3b0f1cb6bae104bfc2d25d1bcab4e964226e
Olhavavruk <85847178+Olhavavruk@users.noreply.github.com>
2021-06-23 14:30:49 +0300

    Create tasks-03.js

---
+++ tasks-03.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+1) Find the smallest integer in the array codewars
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
